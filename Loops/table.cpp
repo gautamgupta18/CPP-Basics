1f16c2a22ff8bf34afc8e504d4384354da39b6b3
gautamgupta18 <gautamkumarmk22@gmail.com>
2026-09-21 17:00:26 +0530

    to print table of any number

--- Loops/table.cpp
+++ Loops/table.cpp
@@ -3,7 +3,7 @@ using namespace std ;
 int main()
 {
     int n ;
-    cout << "Enter nth number to print:" ;
+    cout << "Enter number to print table:" ;
     cin >> n ;
     for ( int i = 1 ; i <= 10 ; i++ )
        cout << n << " * " << i << " = " << n * i << endl ;
