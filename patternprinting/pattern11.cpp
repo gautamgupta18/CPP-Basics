18088b8caf710e88bdec3b3540ad4fa6789abd6b
gautamgupta18 <gautamkumarmk22@gmail.com>
2026-09-24 23:21:22 +0530

    Multiply star pattern

---
+++ patternprinting/pattern11.cpp
@@ -0,0 +1,28 @@
+// Multiply sign star pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout << "Enter a odd number :";
+    cin >> n ;
+    if ( n % 2 != 0)
+    for ( int i = 1 ; i <= n ; i++)
+    {
+        cout << endl ; 
+        for ( int j = 1 ; j <= n ; j++ )
+        {
+            if ( i == j || i + j == n + 1 )
+             cout << "* " ;
+            else
+            cout << "  " ;
+        }
+    }
+}
+// *           * 
+//   *       *   
+//     *   *     
+//       *       
+//     *   *     
+//   *       *   
+// *           * 
