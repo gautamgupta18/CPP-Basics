bfc03a9d37d5fd67f87426e58ecd68bfb12a62cb
gautamgupta18 <gautamkumarmk22@gmail.com>
2026-09-24 21:51:48 +0530

    Number rectangle pattern 1

---
+++ patternprinting/pattern2.cpp
@@ -0,0 +1,18 @@
+// number rectangle pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    int r , c ;
+    cout << "Enter numbers of rows and column :";
+    cin >> r >> c ;
+    for ( int i = 1 ; i <= r ; i++)
+    {
+        cout << endl ; 
+        for ( int j = 1 ; j <= c ; j++ )
+          cout << j << " " ;
+    }
+}
+// 1 2 3 4
+// 1 2 3 4
+// 1 2 3 4
