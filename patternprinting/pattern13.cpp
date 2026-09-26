1ca77bef26c8a421e986721c1829b776ee2ec249
gautamgupta18 <gautamkumarmk22@gmail.com>
2026-09-26 13:38:24 +0530

    Binary triangle pattern

---
+++ patternprinting/pattern13.cpp
@@ -0,0 +1,24 @@
+//Binary triangle pattern 1
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout << "Enter number of column:" ;
+    cin >> n ;
+    for ( int i = 1 ; i <= n ; i++ )
+    {
+        cout << endl ;
+        for ( int j = 1 ; j <= i ; j++ )
+          if ( ( i + j ) % 2 == 0 )
+             cout <<  1 << ' ' ;
+          else
+              cout << 0 << " " ;
+    }    
+
+}
+// 1 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// 1 0 1 0 1 
